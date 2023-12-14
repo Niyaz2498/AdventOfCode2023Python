ad601f86e302f2904f0e4ce46eca4b6d4623e65a
niyaz2498 <mohamedniyazm7@gmail.com>
2023-12-14 22:57:56 +0530

    Solution for day 6 - part 1 and 2. Solution for part 2 took 20 seconds, so need to check if there is a way to optimize.

---
+++ AOC2023/src/day_6/prob_1.py
@@ -0,0 +1,31 @@
+from src.utils.inputParser import convert_file_to_str_array
+import os
+
+# ip_list = convert_file_to_str_array(os.path.abspath('easy.txt'))
+ip_list = convert_file_to_str_array(os.path.abspath('hard.txt'))
+time_list = ip_list[0].split()
+time_list.pop(0)
+speed_list = ip_list[1].split()
+speed_list.pop(0)
+
+ip_map = {}
+l = len(time_list)
+ans = 1
+for i in range(l):
+    ways = 0
+    tot_dut = int(time_list[i])
+    print(tot_dut)
+    tar_speed = int(speed_list[i])
+    for j in range(tot_dut):
+        max_speed = j * (tot_dut - j)
+        if max_speed > tar_speed:
+            break
+        print(j, end=" --> ")
+        print(max_speed)
+        ways += 1
+    print("====")
+    ways *= 2
+    ways = tot_dut - ways + 1
+    ans *= ways
+    print("===")
+print(ans)
